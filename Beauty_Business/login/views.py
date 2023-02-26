44abcefa2d09ca5c6e43fca1c478a26ce89aaf97
diegojojoayandun <3922@holbertonschool.com>
2023-02-25 22:17:10 -0500

    UPLOAD: upload django-python pitch project files to repository

---
+++ Beauty_Business/login/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from management import templates
+#from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, "signup.html", {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('/management/')
+            except IntegrityError:
+                return render(
+                    request, "signup.html", {
+                        'form': UserCreationForm,
+                        'error': 'Usuario ya existe'})
+
+        return render(
+            request, "signup.html", {
+                'form': UserCreationForm, 'error': 'Password No corresponde'})
+
+
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, "login2.html", {'form': AuthenticationForm})
+    else:
+        user = authenticate(request,
+                            username=request.POST['username'],
+                            password=request.POST['password'])
+
+        if user is None:
+            return render(
+                request, 'signin.html', {
+                    'form': AuthenticationForm,
+                    'error': 'nombre de usuario o password incorrectos'})
+        else:
+            login(request, user)
+            print(user.username)
+            return redirect('/management/',user.username)
+
+
+def home(request):
+    return render(request, 'home.html')
