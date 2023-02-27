ceb59ed2599b7ef3fb278164efd60f6658f0b916
diegojojoayandun <3922@holbertonschool.com>
2023-02-27 12:43:37 -0500

    -mUPD: Apply Pep8 in stock viewa

--- Beauty_Business/management/views_stock/views_st.py
+++ Beauty_Business/management/views_stock/views_st.py
@@ -4,9 +4,11 @@ from ..models import Stock
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_page
 
+
 @login_required
 @cache_page(60 * 15)
 def get_stock_all_items(request):
+    '''Get all products available in stock'''
     stock_available = Stock.objects.raw(
         '''SELECT 1 AS stock_id, s.product_id_id,
             sum(s.amount) AS total,
@@ -18,9 +20,10 @@ def get_stock_all_items(request):
         GROUP BY s.product_id_id, p.name_product ORDER BY s.product_id_id'''
     )
     return render(request, "management/stock.html", {
-        'stock_available':stock_available,
+        'stock_available': stock_available,
 
     })
+
 
 @login_required
 def get_stock_by_name(request, name):
@@ -33,13 +36,14 @@ def get_stock_by_name(request, name):
         FROM stock AS s
         INNER JOIN products AS p ON s.product_id_id = p.product_id
         WHERE s.amount > 0 AND p.name_product LIKE %s
-        GROUP BY s.product_id_id, p.name_product ORDER BY s.product_id_id''', [name + '%']
-
+        GROUP BY s.product_id_id, p.name_product
+        ORDER BY s.product_id_id''', [name + '%']
     )
     return render(request, "management/stock.html", {
-        'stock_available':stock_available,
+        'stock_available': stock_available,
 
     })
+
 
 @login_required
 def get_stock_by_id(request, id):
@@ -52,10 +56,11 @@ def get_stock_by_id(request, id):
         FROM stock AS s
         INNER JOIN products AS p ON s.product_id_id = p.product_id
         WHERE s.amount > 0 AND s.product_id_id = %s
-        GROUP BY s.product_id_id, p.name_product ORDER BY s.product_id_id''', [id]
+        GROUP BY s.product_id_id, p.name_product
+        ORDER BY s.product_id_id''', [id]
 
     )
     return render(request, "management/stock.html", {
-        'stock_available':stock_available,
+        'stock_available': stock_available,
 
     })
